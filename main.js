6965c919fc34de8b8a4b7dcd6ebf901e59ac4be7
paperfox <ndg.paperfox@gmail.com>
2024-11-02 16:52:58 -0400

    test pagination working

--- main.js
+++ main.js
@@ -102,3 +102,64 @@ displayArt(data);
 //   // Append the "li" node to the list:
 //   document.getElementById('myList').appendChild(node);
 // }
+
+
+
+
+// let results = data;
+// const resultsPerPage = 10;
+
+// console.log(results)
+
+// const artList = document.querySelector('.art-list')
+// const first = document.querySelector('.first')
+// const prev = document.querySelector('.previous')
+// const next = document.querySelector('.next')
+// const last = document.querySelector('.last')
+
+// // this is setting up the empty array, could be from an api or something
+// let arrayList = []
+// let page = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   let li = document.createElement('li');
+//   li.innerHTML = `<div><strong>List item ${i}</strong></div>
+//   <div>testing divs</div>`
+//   arrayList.push(li);
+// }
+
+// for (let i = 0; i < page+ 10; i++) {
+//   artList.appendChild(arrayList[i]);
+// }
+
+// next.addEventListener('click', function() {
+//   page === arrayList.length - 10 ? (page = 0) : (page+= 10)
+//   artList.innerHTML = '';
+//   for (let i = page; i < page+ 10; i++) {
+//     artList.appendChild(arrayList[i])
+//   }
+// });
+
+// prev.addEventListener('click', function() {
+//   page === 0 ? (page = arrayList.length - 10) : (page-= 10)
+//   artList.innerHTML = '';
+//   for (let i = page; i < page + 10; i++) {
+//     artList.appendChild(arrayList[i])
+//   }
+// });
+
+// first.addEventListener('click', function() {
+//   page = 0;
+//   artList.innerHTML = '';
+//   for (let i = page; i < page + 10; i++) {
+//     artList.appendChild(arrayList[i])
+//   }
+// });
+
+// last.addEventListener('click', function() {
+//   page = arrayList.length - 10
+//   artList.innerHTML = '';
+//   for (let i = page; i < page + 10; i++) {
+//     artList.appendChild(arrayList[i])
+//   }
+// });
