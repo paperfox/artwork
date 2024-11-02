7c3accdef398db97f8fd6098fa2e4d71f31752c9
paperfox <ndg.paperfox@gmail.com>
2024-11-02 14:23:38 -0400

    begin pagination work

--- main.js
+++ main.js
@@ -54,3 +54,55 @@ displayArt(data);
 //   // Append the new page to the document body
 //   document.body.appendChild(newPage);
 // }
+
+
+
+const containerPaginationResults = document.querySelector('.paginaton-content');
+
+let results = data;
+const resultsPerPage = 10;
+
+function pagination(evt, pageName) {
+  const pageContent = document.getElementsByClassName('page-content');
+  const pageControl = document.getElementsByClassName('page-control');
+
+  for (let i = 0; i < pageContent.length; i++) {
+    pageContent[i].className = pageContent[i].className.replace(' active', '');
+    pageControl[i].className = pageControl[i].className.replace(' active', '');
+  }
+
+  document.getElementById(pageName).className += ' active';
+  evt.currentTarget.className += ' active';
+}
+
+
+for (let i = 0; i < results.length; i += resultsPerPage) {
+  let pageResults = results.slice(i, i + resultsPerPage);
+    console.log(pageResults)
+  // let newPage = document.createElement("div");
+  // newPage.classList.add(`page-${i}`)
+
+  let formatted = []
+
+//   Append the results to the new page
+  pageResults.forEach((res, i) => {
+    let html = `<div class="item-${i}">
+      <p>${res.title}</p>
+      <p>${res.completed === true ? 'this is a finished piece' : 'work in progress'}</p>
+    </div>`;
+    // containerPaginationResults.insertAdjacentHTML('afterbegin', html)
+
+    // console.log(html)
+    formatted.push(html)
+  });
+  
+  
+//   Append the new page to the document body
+  // document.containerPaginationResults.appendChild(newPage);
+  console.log(formatted)
+  const owo = `<div class="page-${i}'>${formatted}</div>`
+  console.log(owo)
+  
+  containerPaginationResults.insertAdjacentHTML('afterbegin', owo)
+
+}
