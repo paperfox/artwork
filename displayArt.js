3f08de8151dc03acb0de3275cd14d7733fb5a951
paperfox <ndg.paperfox@gmail.com>
2024-11-02 22:36:37 -0400

    more art...

--- displayArt.js
+++ displayArt.js
@@ -7,13 +7,14 @@ export function displayArt(arts) {
 
   let arrayList = [];
   let page = 0;
+  const paginationValue = 15;
 
-  const pageCount = Math.ceil(arts.length / 10);
+  const pageCount = Math.ceil(arts.length / paginationValue);
   console.log(pageCount);
 
   const paginator = () => {
     artList.innerHTML = '';
-    for (let i = page; i < page + 10; i++) {
+    for (let i = page; i < page + paginationValue; i++) {
       artList.appendChild(arrayList[i]);
     }
   };
@@ -23,25 +24,22 @@ export function displayArt(arts) {
     li.innerHTML = `
       <div class="artpiece-${i}">
         <img src="art/${art.link}" alt="${art.title}: ${art.desc}" />
+        <p>${i}</p>
       </div>`;
     arrayList.push(li);
   });
 
-  for (let i = 0; i < page + 10; i++) {
+  for (let i = 0; i < page + paginationValue; i++) {
     artList.appendChild(arrayList[i]);
   }
 
   next.addEventListener('click', function () {
-    // page === arrayList.length - 10 ? (page = 0) : (page+= 10)
-    page <= pageCount ? (page += 10) : (page = arrayList.length - 10);
-    // the above shows the last 10 when the user hits the last page which isn't ideal
-    // but it fixes my console error and it's ok for now
+page === arrayList.length - paginationValue ? (page = arrayList.length - paginationValue) : (page+= paginationValue)
     paginator();
   });
 
   prev.addEventListener('click', function () {
-    page <= 0 ? (page = 0) : (page -= 10);
-    // page === 0 ? (page = arrayList.length - 10) : (page-= 10)
+    page <= 0 ? (page = 0) : (page -= paginationValue);
     paginator();
   });
 
@@ -51,41 +49,7 @@ export function displayArt(arts) {
   });
 
   last.addEventListener('click', function () {
-    page = arrayList.length - 10;
+    page = arrayList.length - paginationValue;
     paginator();
   });
 }
-
-
-
-// next.addEventListener('click', function() {
-//   page === arrayList.length - 10 ? (page = 0) : (page+= 10)
-//   taskList.innerHTML = '';
-//   for (let i = page; i < page+ 10; i++) {
-//     taskList.appendChild(arrayList[i])
-//   }
-// });
-
-// prev.addEventListener('click', function() {
-//   page === 0 ? (page = arrayList.length - 10) : (page-= 10)
-//   taskList.innerHTML = '';
-//   for (let i = page; i < page + 10; i++) {
-//     taskList.appendChild(arrayList[i])
-//   }
-// });
-
-// first.addEventListener('click', function() {
-//   page = 0;
-//   taskList.innerHTML = '';
-//   for (let i = page; i < page + 10; i++) {
-//     taskList.appendChild(arrayList[i])
-//   }
-// });
-
-// last.addEventListener('click', function() {
-//   page = arrayList.length - 10
-//   taskList.innerHTML = '';
-//   for (let i = page; i < page + 10; i++) {
-//     taskList.appendChild(arrayList[i])
-//   }
-// });
