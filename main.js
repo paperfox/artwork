f93170d6f62c266c427aeee19e989b3d8015d6e5
paperfox <ndg.paperfox@gmail.com>
2024-11-04 20:30:52 -0500

    reorganize files

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import './style.css';
-import { displayArt } from './displayArt.js';
-import { leftRail } from './leftRail.js';
-import dataArt from './artData.json';
-import dataSketches from './sketchData.json';
+import "./style.css";
+import { displayArt } from "./components/displayArt.js";
+import { leftRail } from "./components/leftRail.js";
+import dataArt from "./data/artData.json";
+import dataSketches from "./data/sketchData.json";
 
 // To Do
 // Add all art data
@@ -10,28 +10,28 @@ import dataSketches from './sketchData.json';
 // on clicking pagination, scroll to top of page
 
 export function openTab(evt, tabName) {
-  const tabContent = document.getElementsByClassName('tab-content');
-  const tabLinks = document.getElementsByClassName('tab-control');
+  const tabContent = document.getElementsByClassName("tab-content");
+  const tabLinks = document.getElementsByClassName("tab-control");
 
   for (let i = 0; i < tabContent.length; i++) {
-    tabContent[i].className = tabContent[i].className.replace(' active', '');
-    tabLinks[i].className = tabLinks[i].className.replace(' active', '');
+    tabContent[i].className = tabContent[i].className.replace(" active", "");
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
   }
 
-  // displayArt(dataSketches);
-  document.getElementById(tabName).className += ' active';
-  evt.currentTarget.className += ' active';
+  document.getElementById(tabName).className += " active";
+  evt.currentTarget.className += " active";
 }
 window.openTab = openTab;
 
 // Left rail
 leftRail();
-// Main
 
-// why did this break my modals again?
-const owo2 = document.querySelector('.art-list-sketches');
-displayArt(dataSketches, owo2);
-const owo = document.querySelector('.art-list');
-displayArt(dataArt, owo);
+// Main
+const sketches = document.querySelector(".art-list-sketches");
+displayArt(dataSketches, sketches);
+const finishedWork = document.querySelector(".art-list");
+displayArt(dataArt, finishedWork);
+
+//
 // because I frequently need to know how many art pieces I have I'm leaving this here for now
 // console.log(dataArt.length);
