506495e659a583de1d8bed2466fa7e9d9c49c389
paperfox <ndg.paperfox@gmail.com>
2024-11-04 21:28:48 -0500

    debugging json issue

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "https://paperfox.github.io/artwork/",
-  plugins: [],
-  test: {
-    globals: true,
-    // environment: "jsdom",
-    setupFiles: [],
+  server: {
+    mimeTypes: {
+      "application/json": ["json"],
+    },
   },
 });
